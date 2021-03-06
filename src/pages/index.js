cc7c48011c4a97afcf261647ad986bbcc7dd7309
felipemaxrosa <felipemax.suporteti@gmail.com>
2021-03-06 11:32:02 -0300

    video card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,78 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Layout from "../components/Layout";
+import { Box, Grid } from '@material-ui/core';
+import VideoCard from '../components/VideoCard';
 
-export default function Home() {
+function Home({data}) {
   return (
     <Layout title="Youtube">
-      Clone youtube com Next.js + Material-UI
-      <Button variant="outlined" color="secondary">
-        Primary
-      </Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid> 
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER [PARTE #01]',
+      authorId: 1,
+      authorName: 'Felipe Rosa',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: String(new Date()),
+    },
+    {
+      id: 2,
+      title:
+        'NEXT.JS: ENTENDENDO A RENDERIZAÇÃO REACT NO LADO SERVIDOR [PARTE #02]',
+      authorId: 1,
+      authorName: 'Felipe Rosa',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next02.png',
+      videoUrl: 'url',
+      updatedAt: String(new Date()),
+    },
+    {
+      id: 3,
+      title: 'NEXT.JS: ROTAS ESTÁTICAS E DINÂMICAS [PARTE #03]',
+      authorId: 1,
+      authorName: 'Felipe Rosa',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: String(new Date()),
+    },
+    {
+      id: 4,
+      title: 'NEXT.JS: ROTAS ESTÁTICAS E DINÂMICAS [PARTE #04]',
+      authorId: 1,
+      authorName: 'Felipe Rosa',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: String(new Date()),
+    },
+  ];
+
+  return {
+    props: {
+      data: data
+    }, // will be passed to the page component as props
+  };
+}
+
+export default Home;
