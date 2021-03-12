7f6c240ef324b0aa6e8f7fcd783879b6c0bbd3b0
felipemaxrosa <felipemax.suporteti@gmail.com>
2021-03-12 15:33:47 -0300

    get video by id

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Box, Typography, Avatar, makeStyles } from '@material-ui/core';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import { useRouter } from 'next/router';
+
 
 dayjs.extend(relativeTime);
 
@@ -20,10 +22,16 @@ const useStyles = makeStyles((theme) => ({
 
 function VideoCard({ item }) {
   const classes = useStyles();
+  const router = useRouter();
 
   return (
     <Box>
-      <img alt={item.title} src={item.thumb} className={classes.img} />
+      <img alt={item.title} src={item.thumb} className={classes.img}
+        onClick={() => router.push({
+          pathname: "/video/[id]",
+          query: {id: item._id}
+        })}
+      />
       <Box display="flex" mt={1}>
         <Box mr={2}>
           <Avatar alt={item.authorName} src={item.authorAvatar}>
